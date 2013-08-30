a4087ce642c89f6730094eb685f4738246914248
masterchief117 <bstewart@Michaels-Mac-mini-2.local>
2013-08-30 11:17:08 -0700

    Have project up and running in the mavens

---
+++ src/test/java/com/animalclan/donkey/HelloAndroidActivityTest.java
@@ -0,0 +1,22 @@
+package com.animalclan.donkey;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by bstewart on 8/30/13.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class HelloAndroidActivityTest {
+
+    public void someTest(){
+
+    }
+
+    @Test
+    public void HelloAndroidActivityTest(){
+        Assert.assertTrue(true);
+    }
+}
